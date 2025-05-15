f24ab2dfd22ef41d44587362973a92202ee8fb40
George Jimy <knivespeans@gmail.com>
2025-05-15 16:42:19 -0500

    Lab

---
+++ Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zappar;
+
+public class TouchHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    private bool isSplit;
+    [SerializeField]
+    private ZapparInstantTrackingTarget m_instantTracker;
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (animator != null && Input.GetMouseButtonDown(0))
+#else
+        if (animator != null && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+#endif
+        {
+            if (m_instantTracker.UserHasPlaced)
+                isSplit = !isSplit;
+                animator.SetBool("isSplit",isSplit);
+        }
+    }
+}
